9d5dcae814f66e4f1fb99d6e18502a5bfa41d17f
Sachin Kumar <sachinkumar22018472@gmail.com>
2026-08-20 04:25:39 +0530

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import about from '../assets/about.png'
-import NewLetterBox from '../components/newLetterBox'
+import NewLetterBox from "../components/NewLetterBox";
 import Footer from '../components/Footer'
 
 function About() {
