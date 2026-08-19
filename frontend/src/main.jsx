d9cf8576e3ce0bf13c73a89ddfbb05be9beadf5e
Sachin Kumar <sachinkumar22018472@gmail.com>
2026-08-20 04:09:36 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import AuthContext from './context/authContext.jsx'
+import AuthContext from "./context/AuthContext.jsx";
 import UserContext from './context/UserContext.jsx'
 import ShopContext from './context/ShopContext.jsx'
 
