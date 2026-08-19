6499ce8ea988c7b7406fd4a8d8e355fc2249a343
Sachin Kumar <sachinkumar22018472@gmail.com>
2026-08-20 04:37:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(
     cors({
         origin: [
-            "http://localhost:5173",
+            "https://ecommerce-ai-voicechatbotfrontend.onrender.com",
             "http://localhost:5174"
         ],
         credentials: true
